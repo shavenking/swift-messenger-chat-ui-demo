574b105b55857cb442bc5c59e9af5083660637d1
shavenking <shavenking@gmail.com>
2018-03-13 14:13:31 +0800

    Initial

---
+++ chat-ui-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat-ui-demo
+//
+//  Created by Andy Tsai on 13/03/2018.
+//  Copyright © 2018 Andy Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
